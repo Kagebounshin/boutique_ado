fcb77d30ebc25f2227fe2e2f55184d3b4176acd8
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2021-11-14 08:43:34 +0000

    Added much

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,8 +1,8 @@
+
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
-
 
 def bag_contents(request):
 
@@ -23,7 +23,7 @@ def bag_contents(request):
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data['item_by_size'].items():
+            for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
@@ -34,14 +34,14 @@ def bag_contents(request):
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PRECENTAGE / 100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
-    else: 
+    else:
         delivery = 0
         free_delivery_delta = 0
     
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
@@ -50,7 +50,6 @@ def bag_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
-
     }
 
     return context
